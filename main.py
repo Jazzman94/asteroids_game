a458b2bab662e4131238f2094a434a6dc7f01725
Jiří Jaskowiec <jirijaskowiec@seznam.cz>
2025-04-02 09:28:13 +0200

    feat: add player class

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    clock = pygame.time.Clock() 
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+
+
+
 
     # In your game loop
     while True:
@@ -20,11 +25,13 @@ def main():
                 return
                 
         screen.fill((0, 0, 0))  # Fill the screen instance, not the class
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(SCREEN_FPS)/1000
 
 
 
         
-
 if __name__ == "__main__":
     main()
