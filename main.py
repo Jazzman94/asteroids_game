09ce3d3a15c5bc0db0190f37d121754546a19cae
Jiří Jaskowiec <jirijaskowiec@seznam.cz>
2025-04-02 10:19:43 +0200

    feat: adds group func

--- main.py
+++ main.py
@@ -9,27 +9,29 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
     clock = pygame.time.Clock() 
     dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-
-
-
-
 
     # In your game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
         screen.fill((0, 0, 0))  # Fill the screen instance, not the class
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
 
         pygame.display.flip()
         dt = clock.tick(SCREEN_FPS)/1000
-
 
 
         
